ce77432cf572170a9922ece2a81edd9fff36caeb
Thiago Wyse <contatothiagowyseoficial@gmail.com>
2025-05-24 10:24:20 -0300

    Initial commit

---
+++ src/main/java/com/projetojudo/judotexeira/JudotexeiraApplication.java
@@ -0,0 +1,13 @@
+package com.projetojudo.judotexeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudotexeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudotexeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetojudo/judotexeira/JudotexeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojudo.judotexeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudotexeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
